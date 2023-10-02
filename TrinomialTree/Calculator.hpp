ebce8ae8fe479c1971d3a6eec2aebd30abcd8416
Eric Zeng <ericzeng@Erics-MBP.lan>
2023-10-02 07:48:30 -0400

    cpp of tree models

---
+++ TrinomialTree/Calculator.hpp
@@ -0,0 +1,23 @@
+#ifndef Calculator_hpp
+#define Calculator_hpp
+
+#include <iostream>
+#include <iomanip>
+#include "TrinomialTree.hpp"
+#include "BinomialTreeVolatilityUnknown.hpp"
+#include "TrinomialTreeVolatilityUnknown.hpp"
+#include "BinomialTree.hpp"
+#include "EuropeanOption.hpp"
+
+
+class Calculator
+{
+public:
+    Calculator(){};
+    ~Calculator(){};
+    
+    void PriceAndGreeks(const EuropeanOption& option, bool american);
+    void PriceAndGreeks_compensated(const EuropeanOption& option);
+};
+
+#endif // Calculator.hpp
