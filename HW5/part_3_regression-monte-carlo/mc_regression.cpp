d355200ad232bf95b724559aa9bc188765e47579
Eric Zeng <ericzeng@Erics-MBP.lan>
2023-10-09 07:16:01 -0400

    Part 3.1

--- HW5/part_3_regression-monte-carlo/mc_regression.cpp
+++ HW5/part_3_regression-monte-carlo/mc_regression.cpp
@@ -46,7 +46,6 @@ namespace montecarlo {
             return evaluate_polynomial(_coeffs.at(time), x);
         }
 
-        // Part 3.1
         /* Implement a constructor similar to that of PolynomialMCRegression */
         HermiteMCRegression::HermiteMCRegression(const uint times, const uint degree, const bool precondition,
                                                        const bool standardize):
@@ -59,18 +58,45 @@ namespace montecarlo {
             _stds.resize(times);
         }
 
+        // Part 3.1
+        /* Implement a Hermite version of PolynomialMCRegression::fit_predict */
         arr HermiteMCRegression::fit_predict(const uint time, const arr &x, const arr &y) {
-			/* Implement a Hermite version of PolynomialMCRegression::fit_predict */
             if (time == 0) {
                 throw std::out_of_range ("At time 0, fit_predict_at_0 should be used.");
             }
+            
+            arr result;
+            if (standardize)
+            {
+                arr2 matrix_H; // the standardized Vandermonde matrix for input values of X and a given degree of hermite polynomial
+                std::tie(matrix_H, _means.at(time), _stds.at(time)) = hermite_vandermonde_standardized(x, degree);
+                std::tie(_coeffs.at(time), result) = fit_linear_regression(matrix_H, y, precondition);
+            }
+            else
+            {
+                std::tie(_coeffs.at(time), result) = fit_linear_regression(hermite_vandermonde(x, degree), y, precondition);
+            }
+
+            return result;
         }
 
+        // Part 3.1
+        /* Implement a Hermite version of PolynomialMCRegression::predict */
         arr HermiteMCRegression::predict(const uint time, const arr& x) const {
-            /* Implement a Hermite version of PolynomialMCRegression::predict */
             if (time == 0) {
                 throw std::out_of_range ("At time 0, fit_predict_at_0 should be used.");
             }
+
+            arr result;
+            if (standardize)
+            {
+                result = evaluate_hermite_polynomial_standardized(_coeffs.at(time), x, _means.at(time), _stds.at(time));
+            }
+            else
+            {
+                result = evaluate_hermite_polynomial(_coeffs.at(time), x);
+            }
+            return result;
         }
     }
 }
